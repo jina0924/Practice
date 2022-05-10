adf3c0da8b43d44dccde9ca1fc56f8bf9e8961be
jina jeong <jina092400@gmail.com>
2022-05-10 17:15:15 +0900

    Solve Baekjoon 17073

---
+++ 03_01_algorithm_practice/Baekjoon/17073_나무위의빗물/s2.py
@@ -0,0 +1,17 @@
+# 백준 17073번 나무 위의 빗물 - 시간 초과
+
+import sys
+sys.stdin = open('input.txt')
+
+
+N, W = map(int, sys.stdin.readline().split())        # N: 노드의 수, W: 1번 노드에 고인 물의 양
+node = [0] * (N+1)
+for _ in range(N-1):
+    U, V = map(int, sys.stdin.readline().split())
+    node[U] += 1
+    node[V] += 1
+leaf_cnt = 0
+for i in range(2, N+1):
+    if node[i] == 1:
+        leaf_cnt += 1
+print(W / leaf_cnt)
