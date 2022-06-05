fdc639b5f1da4ed6135ee3ce92623dc5af9bf633
jina jeong <jina092400@gmail.com>
2022-06-05 22:46:12 +0900

    Try Baekjoon 1992

---
+++ 03_01_algorithm_practice/Baekjoon/1992_쿼드트리/s1.py
@@ -0,0 +1,54 @@
+# 백준 1992번 쿼드트리
+
+import sys
+sys.stdin = open('input1.txt')
+
+
+def compress(r1, c1, r2, c2):
+    if r2 - r1 <= 1 and c2 - c1 <= 1:
+        return
+    lt = data[r1][c1]
+    rt = data[r1][c2]
+    lb = data[r2][c1]
+    rb = data[r2][c2]
+    same = True
+    for r in range(r1, (r1 + r2) // 2 + 1):
+        for c in range(c1, (c1 + c2) // 2 + 1):
+            if data[r][c] != lt:
+                same = False
+    if not same:
+        compress(r1, c1, (r1 + r2) // 2, (c1 + c2) // 2)
+    else:
+        print(f'({lt}', end='')
+    same = True
+    for r in range(r1, (r1 + r2) // 2 + 1):
+        for c in range((c1 + c2) // 2 + 1, c2 + 1):
+            if data[r][c] != rt:
+                same = False
+    if not same:
+        compress(r1, (c1 + c2) // 2 + 1, (r1 + r2) // 2, c2)
+    else:
+        print(f'{rt}', end='')
+    same = True
+    for r in range((r1+ r2) // 2 + 1, r2 + 1):
+        for c in range(c1, (c1 + c2) // 2 + 1):
+            if data[r][c] != lb:
+                same = False
+    if not same:
+        compress((r1 + r2) // 2 + 1, c1, r2, (c1 + c2) // 2)
+    else:
+        print(f'{lb}', end='')
+    same = True
+    for r in range((r1 + r2) // 2 + 1, r2 + 1):
+        for c in range((c1 + c2) // 2 + 1, c2 + 1):
+            if data[r][c] != rb:
+                same = False
+    if not same:
+        compress((r1 + r2) // 2 + 1, (c1 + c2) // 2 + 1, r2, c2)
+    else:
+        print(f'{rb})', end='')
+
+
+N = int(input())
+data = [list(map(int, input())) for _ in range(N)]
+compress(0, 0, N-1, N-1)
