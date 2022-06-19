c260de1a5ddca82e1c34ed182c787ca0a0566a7f
jina jeong <jina092400@gmail.com>
2022-06-19 23:36:29 +0900

    Try Baekjoon 2293

--- 01_algorithm_practice/Baekjoon/2293_동전1/s1.py
+++ 01_algorithm_practice/Baekjoon/2293_동전1/s1.py
@@ -4,17 +4,23 @@ import sys
 sys.stdin = open('input.txt')
 
 
+def func(res, i):
+    global cnt
+    if i >= n:
+        return
+    res %= coin[i]
+    if res == 0:
+        cnt += 1
+    elif res < coin[i]:
+        func(res, i + 1)
+    func(res + coin[i], i + 1)
+
+
 n, k = map(int, input().split())
 coin = []
 for _ in range(n):
     coin.append(int(input()))
 coin.sort(reverse=True)
-idx = cnt = 0
-while idx < n:
-    k -= coin[idx]
-    if k == 0:
-        cnt += 1
-        k += coin[idx]
-        idx += 1
-    elif k < coin[idx]:
-        idx += 1
+cnt = 0
+func(k, 0)
+print(cnt)
