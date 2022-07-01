26668d0242f19300551edd9d0756223bff1a125e
jina jeong <jina092400@gmail.com>
2022-07-01 23:50:11 +0900

    Solve BOJ 1937

--- 01_algorithm_practice/Baekjoon/[BOJ]1937_욕심쟁이판다/s3.py
+++ 01_algorithm_practice/Baekjoon/[BOJ]1937_욕심쟁이판다/s3.py
@@ -3,13 +3,6 @@
 import sys
 sys.stdin = open('input.txt')
 
-dr, dc = (-1, 1, 0, 0), (0, 0, -1, 1)
-
-
-def dfs():
-    pass
-
-
 n = int(input())
 forest = []
 bamboo = []
@@ -18,6 +11,19 @@ for r in range(n):
     for c in range(n):
         bamboo.append((data[c], r, c))
     forest.append(data)
-bamboo.sort()
-print(forest)
-print(bamboo)
+bamboo.sort(reverse=True)
+result = [[1] * n for _ in range(n)]
+i = 0
+ans = 1
+dr, dc = (-1, 1, 0, 0), (0, 0, -1, 1)
+while i < len(bamboo):
+    w, r, c = bamboo[i]
+    for d in range(4):
+        nr, nc = r + dr[d], c + dc[d]
+        if 0 <= nr < n and 0 <= nc < n and forest[nr][nc] < w:
+            if result[nr][nc] < result[r][c] + 1:
+                result[nr][nc] = result[r][c] + 1
+            if ans < result[nr][nc]:
+                ans = result[nr][nc]
+    i += 1
+print(ans)
