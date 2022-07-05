8361886a84e72224013726da98634353abe80d9c
jina jeong <jina092400@gmail.com>
2022-07-05 10:43:28 +0900

    Solve BOJ 10986

--- 01_algorithm_practice/Baekjoon/[BOJ]10986_나머지합/s1.py
+++ 01_algorithm_practice/Baekjoon/[BOJ]10986_나머지합/s1.py
@@ -8,22 +8,23 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 data = list(map(int, input().split()))
 
-for i in range(N):
-    data[i] %= M
+remainder = [0] * M                 # 나머지는 0 ~ M-1
 
-result = [data[0]]
-i, cnt = 1, 0
-if data[0] == 0:
-    cnt += 1
-while i < N:
-    if data[i] == 0:
-        cnt += 1
-    temp = []
-    for res in result:
-        new_res = (res + data[i]) % M
-        if new_res == 0:
-            cnt += 1
-        temp.append(new_res)
-    result += temp
-    i += 1
+remainder[data[0] % M] += 1         # 누적합 하기 전에 맨 처음 값의 나머지 먼저 카운팅 해놓기
+
+for i in range(1, N):
+    data[i] += data[i-1]            # 첫 번째 값부터 현재 위치 값까지 누적합
+    remainder[data[i] % M] += 1     # 누적합의 나머지 카운팅
+
+cnt = remainder[0]                  # 누적합의 나머지가 0인 부분 먼저 카운팅에 올려둠
+
+'''
+위에서 구한 누적합 : 0 ~ i까지 전부 더한 값
+나머지가 같은 누적합 중 2개 고름 ex) 0 ~ 1, 0 ~ 4 누적합 고름
+=> 두 누적합을 빼게 되면 나머지가 0인 구간 합이 됨 ex) 1 ~ 4 구간합이 0이 됨
+=> remainder에서 두 개를 조합하면 구간합 0 만들어짐
+'''
+for r in remainder:
+    cnt += r * (r-1) // 2
+
 print(cnt)
