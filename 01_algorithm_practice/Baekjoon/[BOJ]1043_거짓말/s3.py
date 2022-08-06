ad01db7faa9322552dc3396d50d1d0c08452e468
jina jeong <jina092400@gmail.com>
2022-08-07 00:03:00 +0900

    Solve BOJ 1043

--- 01_algorithm_practice/Baekjoon/[BOJ]1043_거짓말/s3.py
+++ 01_algorithm_practice/Baekjoon/[BOJ]1043_거짓말/s3.py
@@ -5,26 +5,20 @@ sys.stdin = open('input8.txt')
 input = sys.stdin.readline
 
 
-N, M = map(int, input().split())
-already = set(input().split()[1:])
-parties = []
+N, M = map(int, input().split())        # N: 사람의 수, M: 파티의 수
+already = set(input().split()[1:])      # set으로 진실을 아는 사람들 저장해둠
 
-# for _ in range(M):
-#     parties.append(set(input().split()[1:]))
+parties = [set(input().split()[1:]) for _ in range(M)]      # 각 파티에 오는 사람들 정보
 
-for num in range(M):
-    party = set(input().split()[1:])
-    if party & already:
-        already = already | party
-    parties.append(party)
-    # for j in range(num+1):
-    i = 0
-    while i < num+1:
-        if parties[i] & already:
-            already = already | parties[i]
-            i = 0
-        else:
-            i += 1
+'''
+M번 반복하는 이유: 
+맨 마지막에 already에 추가된다면 모든 파티를 다시 돌면서 사람들을 확인해야 함
+'''
+for _ in range(M):
+    for party in parties:
+        if party & already:
+            already = already | party
+
 ans = 0
 for i in range(M):
     if not set(parties[i]) & already:
