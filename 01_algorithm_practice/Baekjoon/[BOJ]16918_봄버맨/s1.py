6cbc8ec152c50b9c1cb94e656d3c5114d8d200b6
jina jeong <jina092400@gmail.com>
2022-10-29 23:45:38 +0900

    Solve BOJ 16918

--- 01_algorithm_practice/Baekjoon/[BOJ]16918_봄버맨/s1.py
+++ 01_algorithm_practice/Baekjoon/[BOJ]16918_봄버맨/s1.py
@@ -1,34 +1,34 @@
-# 백준 16918번 봄버맨
+# 백준 16918번 봄버맨 - x
 
 import sys
-sys.stdin = open('input1.txt')
+sys.stdin = open('input4.txt')
 input = sys.stdin.readline
 
 
 def bomberman(s):
-    if s % 2:
+    if s % 2 == 0 or s % 4 == 3:
         for r in range(R):
             for c in range(C):
-                if data[r][c] == 'o':
-                    data[r][c] = 1
-                else:
-                    data[r][c] = 0
-    else:
-        for r in range(R):
-            for c in range(C):
-                if data[r][c] > 0:
-                    data[r][c] = '.'
-                    for d in range(4):
-                        nr, nc = r + dr[d], c + dc[d]
-                        if 0 <= nr < R and 0 <= nc < C:
-                            data[nr][nc] = '.'
-                else:
-                    data[r][c] += 1
+                data[r][c] = 'O'
+        if s % 4 == 3:
+            for b in bomb:
+                r, c = b
+                data[r][c] = '.'
+                for d in range(4):
+                    nr, nc = r + dr[d], c + dc[d]
+                    if 0 <= nr < R and 0 <= nc < C:
+                        data[nr][nc] = '.'
 
 
 R, C, N = map(int, input().split())
-data = [list(input().rstrip()) for _ in range(R)]
+data = []
+bomb = []
+for r in range(R):
+    data.append(list(input().rstrip()))
+    for c in range(C):
+        if data[r][c] == 'O':
+            bomb.append((r, c))
 dr, dc = (-1, 1, 0, 0), (0, 0, -1, 1)
-print(data)
-for s in range(N):
-    bomberman(s)
+bomberman(N)
+for r in range(R):
+    print(*data[r], sep='')
