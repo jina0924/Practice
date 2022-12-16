fd10243fffb258649158c332e4a37720422cd4c0
jina jeong <jina092400@gmail.com>
2022-12-16 22:36:45 +0900

    Solve BOJ 3187

---
+++ 01_algorithm_practice/Baekjoon/[BOJ]3187_양치기꿍/s1.py
@@ -0,0 +1,42 @@
+# 백준 3187번 양치기 꿍
+
+import sys
+sys.stdin = open('input3.txt')
+input = sys.stdin.readline
+
+dr, dc = (-1, 1, 0, 0), (0, 0, -1, 1)
+def bfs(r, c):
+    queue = [(r, c)]
+    wolf, sheep = 0, 0
+    if data[r][c] == 'v':
+        wolf += 1
+    elif data[r][c] == 'k':
+        sheep += 1
+    data[r][c] = '#'
+
+    while queue:
+        cr, cc = queue.pop(0)
+        for d in range(4):
+            nr, nc = cr + dr[d], cc + dc[d]
+            if 0 <= nr < R and 0 <= nc < C and data[nr][nc] != '#':
+                queue.append((nr, nc))
+                if data[nr][nc] == 'v':
+                    wolf += 1
+                elif data[nr][nc] == 'k':
+                    sheep += 1
+                data[nr][nc] = '#'
+    return wolf, sheep
+
+
+R, C = map(int, input().split())
+data = [list(input().rstrip()) for _ in range(C)]
+total_v, total_k = 0, 0
+for r in range(R):
+    for c in range(C):
+        if data[r][c] == 'k' or data[r][c] == 'v':
+            v, k = bfs(r, c)
+            if k > v:
+                total_k += k
+            else:
+                total_v += v
+print(total_k, total_v)
