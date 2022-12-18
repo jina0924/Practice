3d05f404d80b45dbae28c5f1eace47a50abf4520
jina jeong <jina092400@gmail.com>
2022-12-18 21:43:34 +0900

    Solve BOJ 1092

--- 01_algorithm_practice/Baekjoon/[BOJ]1092_배/s1.py
+++ 01_algorithm_practice/Baekjoon/[BOJ]1092_배/s1.py
@@ -3,18 +3,21 @@
 import sys
 sys.stdin = open('input4.txt')
 
-N = int(input())                            # N: 크레인 수
-crane = sorted(list(map(int, input().split())))     # 크레인의 무게 제한
-M = int(input())                            # M: 박스 수
-box = sorted(list(map(int, input().split())))       # 각 박스의 무게
-i, j = N-1, M-1
+N = int(input())    # N: 크레인 수
+crane = sorted(list(map(int, input().split())), reverse=True)     # 크레인의 무게 제한
+M = int(input())    # M: 박스 수
+box = sorted(list(map(int, input().split())), reverse=True)       # 각 박스의 무게
+
+if crane[0] < box[0]:
+    print(-1)
+    sys.exit()
+
 cnt = 0
-while j >= 0:
-    if box[j] <= crane[i]:
-        box[j] = 0
-        j -= 1
-    i -= 1
-    if i < 0 or j < 0:
-        i = N-1
-        cnt += 1
+while len(box):
+    for c in crane:
+        for b in box:
+            if c >= b:
+                box.remove(b)
+                break
+    cnt += 1
 print(cnt)
