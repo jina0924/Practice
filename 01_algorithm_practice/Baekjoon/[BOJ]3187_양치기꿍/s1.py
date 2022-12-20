1c600c49d939904841e99a133a11e12e1dbc0d38
jina jeong <jina092400@gmail.com>
2022-12-20 23:15:58 +0900

    Update comment at BOJ 3187, 1092, 14621

--- 01_algorithm_practice/Baekjoon/[BOJ]3187_양치기꿍/s1.py
+++ 01_algorithm_practice/Baekjoon/[BOJ]3187_양치기꿍/s1.py
@@ -8,11 +8,11 @@ dr, dc = (-1, 1, 0, 0), (0, 0, -1, 1)
 def bfs(r, c):
     queue = [(r, c)]
     wolf, sheep = 0, 0
-    if data[r][c] == 'v':
+    if data[r][c] == 'v':                               # 해당 울타리 안에 있는 늑대와 양의 수를 셈
         wolf += 1
     elif data[r][c] == 'k':
         sheep += 1
-    data[r][c] = '#'
+    data[r][c] = '#'                                    # 살펴본 위치의 값은 '#'으로 채워넣음(다시 보지 않기 위해)
 
     while queue:
         cr, cc = queue.pop(0)
@@ -28,14 +28,14 @@ def bfs(r, c):
     return wolf, sheep
 
 
-R, C = map(int, input().split())
+R, C = map(int, input().split())                        # R, C: 영역의 세로, 가로
 data = [list(input().rstrip()) for _ in range(R)]
-total_v, total_k = 0, 0
+total_v, total_k = 0, 0                                 # total_v, total_k: 살아남은 늑대, 양의 수
 for r in range(R):
     for c in range(C):
-        if data[r][c] == 'k' or data[r][c] == 'v':
-            v, k = bfs(r, c)
-            if k > v:
+        if data[r][c] == 'k' or data[r][c] == 'v':      # 울타리 안에서 양이나 늑대를 발견한다면
+            v, k = bfs(r, c)                            # bfs로 울타리 탐색
+            if k > v:                                   # 남은 개수가 많은 쪽만 살아남음
                 total_k += k
             else:
                 total_v += v
