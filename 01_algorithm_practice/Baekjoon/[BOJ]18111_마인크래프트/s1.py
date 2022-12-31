c21c7dfc9839306bb7a34df1d03c23fcd01f9f3a
jina jeong <jina092400@gmail.com>
2022-12-31 22:55:56 +0900

    Try to solve BOJ 18111

--- 01_algorithm_practice/Baekjoon/[BOJ]18111_마인크래프트/s1.py
+++ 01_algorithm_practice/Baekjoon/[BOJ]18111_마인크래프트/s1.py
@@ -3,13 +3,37 @@
 import sys
 sys.stdin = open('input1.txt')
 
+
+def flatten(height):
+    global ans, top
+    cnt, block = 0, B
+    isFlatten = True
+    for r in range(N):
+        for c in range(M):
+            if ground[r][c] > height:
+                cnt += 2 * (ground[r][c] - height)
+                block += ground[r][c] - height
+            elif ground[r][c] < height:
+                cnt += height - ground[r][c]
+                if block <= 0:
+                    isFlatten = False
+                    flatten(height - 1)
+                else:
+                    block -= height - ground[r][c]
+    if isFlatten and ans > cnt:
+        ans = cnt
+        top = height
+
+
 N, M, B = map(int, input().split())
 ground = []
-top = B
+total = 0
+ans, top = 987987987, 0
 for r in range(N):
     data = list(map(int, input().split()))
-    for c in range(M):
-        if data[c] < top:
-            top = data[c]
+    total += sum(data)
     ground.append(data)
-print(ground, top)
+total /= (N * M)
+flatten(round(total))
+print(ans, top)
+
