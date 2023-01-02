7f9856e2b308ab465cfc87fd807f8a81b17c0ef9
jina jeong <jina092400@gmail.com>
2023-01-03 00:55:50 +0900

    Try to solve BOJ 10216

--- 01_algorithm_practice/Baekjoon/[BOJ]10216_Count_Circle_Groups/s1.py
+++ 01_algorithm_practice/Baekjoon/[BOJ]10216_Count_Circle_Groups/s1.py
@@ -8,6 +8,8 @@ input = sys.stdin.readline
 T = int(input())
 for tc in range(T):
     N = int(input())
-    data = [[0] * N for _ in range(N)]
-    x, y, R = map(int, input().split())
-    data[x][y] = 1
+    # data = [[0] * N for _ in range(N)]
+    for n in range(1, N + 1):
+        x, y, R = map(int, input().split())
+        data[N - y - 1][x] = n
+    print(data)
