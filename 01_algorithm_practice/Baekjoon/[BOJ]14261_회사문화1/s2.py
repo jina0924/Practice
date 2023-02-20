9c337761aed0a8363d79d59e6d3513ad5052fa6f
jina jeong <jina092400@gmail.com>
2023-02-20 16:49:40 +0900

    Solve BOJ 14267

--- 01_algorithm_practice/Baekjoon/[BOJ]14261_회사문화1/s2.py
+++ 01_algorithm_practice/Baekjoon/[BOJ]14261_회사문화1/s2.py
@@ -1,16 +1,18 @@
-# 백준 14267번 회사 문화1 - 시간초과
+# 백준 14267번 회사 문화1 - recursion error
 
 import sys
-sys.stdin = open('input.txt')
+sys.stdin = open('input3.txt')
 input = sys.stdin.readline
 from collections import defaultdict
 
 
-def temp(v):
+def sol(v):
     if v <= 1:
         return 0
     else:
-        return praise[v] + temp(data[v - 1])
+        result = praise[v] + sol(data[v - 1])
+        ans[v] = result
+        return result
 
 
 n, m = map(int, input().split())
@@ -22,8 +24,8 @@ ans = [0] * (n+1)
 praise = [0] * (n+1)
 for _ in range(m):
     i, w = map(int, input().split())
-    praise[i] = w
-for j in range(n, 0, -1):
+    praise[i] += w
+for j in range(n, 1, -1):
     if not ans[j]:
-        ans[j] = temp(j)
+        ans[j] = sol(j)
 print(*ans[1:])
