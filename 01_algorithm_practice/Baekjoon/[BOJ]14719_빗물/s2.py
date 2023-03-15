712c76137c078148bcd12965294c854f5ad72994
jina0924 <jina092400@gmail.com>
2023-03-15 16:28:24 +0900

    Update BOJ 14719 sol

--- 01_algorithm_practice/Baekjoon/[BOJ]14719_빗물/s2.py
+++ 01_algorithm_practice/Baekjoon/[BOJ]14719_빗물/s2.py
@@ -1,22 +1,22 @@
 # 백준 14719번 빗물
 
 import sys
-sys.stdin = open('input3.txt')
+sys.stdin = open('input2.txt')
 
 H, W = map(int, input().split())
 blocks = list(map(int, input().split()))
 
 ans = 0
-for h in range(H):
-    nc, cnt = False, 0
+for h in range(1, H + 1):
+    left, cnt = False, 0
     for c in range(W):
         if blocks[c] >= h:
-            nc = True
+            left = True
             if cnt > 0:
                 ans += cnt
                 cnt = 0
         else:
-            if nc:
+            if left:
                 cnt += 1
 
 print(ans)
